2af2c8b24b8d19a35d18e7f893d304581b29f961
RedCrafter07 <github@redcrafter07.de>
2022-01-29 18:04:58 +0100

    Getting streamer infos powered by EzTw

---
+++ pages/api/info.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import eztw from "eztw";
+let client = new eztw.client()
+if(process.env && process.env.id && process.env.token) client.setID(process.env.id).setToken(process.env.token);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if(!req.headers.streamer || typeof req.headers.streamer != "string") {
+    res.status(400).json({
+      error: 'no streamer'
+    });
+    return;
+  }
+
+  let resp = await eztw.utils.getStreamInfo(req.headers.streamer, client)
+
+  if(resp.thumbnail) resp.thumbnail = eztw.utils.thumbnailSize(resp.thumbnail, 1920, 1080)
+
+  res.status(200).json({ data:resp })
+}
