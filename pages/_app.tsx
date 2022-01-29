14cc76bc5ccd2020ab8d601c5e41713e30d0d2f5
RedCrafter07 <github@redcrafter07.de>
2022-01-29 18:02:20 +0100

    Add footer by default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,22 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer className="flex h-24 w-full items-center justify-center border-t">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
+        </a>
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
